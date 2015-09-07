7c1230388b605d0fcc3e1a40219b8137f510c594
Steffen Klee <steffen.klee@gmail.com>
2015-09-07 07:30:09 +0200

    Fixed protocol generation with multiple files
    CMake expects a list of files to be separated by semi-colon

--- tools/protocol_list.py
+++ tools/protocol_list.py
@@ -9,6 +9,6 @@ with open(argv[1], "r") as f:
         if line[:1] == "#":
             continue
         line = line.strip()
-        outp += include_dir+line
+        outp += include_dir+line+";"
 
-print(outp, end="")
+print(outp[:-1], end="")
