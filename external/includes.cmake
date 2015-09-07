1e2ec5cd9c848ab4b0fcb59a47f70823baf923b9
Steffen Klee <steffen.klee@gmail.com>
2015-09-07 22:32:17 +0200

    Added OpenSSL as a CMake ExternalProject
    OpenSSL will be compiled automatically (cloned form official git repository) with custom flags deactivating unnecessary features. It's libraries are provided as CMake targets.
    A simple add_dependencies(..) plus target_link_libraries(..) is enough.
    
    CMake dependency: openssl     <for add_dependencies(..)>
          libraries: ssl crypto   <for target_link_libraries(..)>

--- external/includes.cmake
+++ external/includes.cmake
@@ -2,4 +2,5 @@ set(EXT ${CMAKE_SOURCE_DIR}/external)
 
 macro(config_compiler_and_linker)
     include_directories(${EXT}/googletest/include)
+    include_directories(${CMAKE_BINARY_DIR}/include)
 endmacro()
