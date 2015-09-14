a7fb09d2e06d67511a67965763889eff87d95c09
Steffen Klee <steffen.klee@gmail.com>
2015-09-14 17:25:56 +0200

    Fixed template mess

--- src/BlockCache.cpp
+++ src/BlockCache.cpp
@@ -4,37 +4,37 @@
 
 #include "libCom/BlockCache.h"
 
-
-BlockInfo::BlockInfo() : address(0) { }
+template<class T>
+BlockInfo<T>::BlockInfo() : address(0) { }
 
 template<class T>
-BlockInfo::BlockInfo(const T address) : address(address) { }
+BlockInfo<T>::BlockInfo(const T address) : address(address) { }
 
 // ##
-
-BlockCache::BlockCache(const uint32_t capacity) : mCapacity(capacity) { }
+template<class K, class V>
+BlockCache<K, V>::BlockCache(const uint32_t capacity) : mCapacity(capacity) { }
 
 template<class K, class V>
-const V BlockCache::read(const K id) const {
+const V BlockCache<K, V>::read(const K id) const {
     return mMap.find(id) != mMap.end() ? mMap.find(id)->second.address : V();
 }
 
 template<class K, class V>
-const uint16_t BlockCache::generation(const K id) const {
+const uint16_t BlockCache<K, V>::generation(const K id) const {
     return mMap.find(id) != mMap.end() ? mMap.find(id)->second.generation : 0;
 }
 
 template<class K, class V>
-bool BlockCache::write(const K id, const V address) {
+bool BlockCache<K, V>::write(const K id, const V address) {
     // element is present
     auto e = mMap.find(id);
     if (e != mMap.end()) {
         // increase generation
-        BlockInfo &block = e->second;
+        BlockInfo<V> &block = e->second;
         // find all elements with same generation
         auto range = mGenMap.equal_range(block.generation);
         // sadly we cannot use "for (auto &el: range)" here..
-        for (std::unordered_multimap<uint16_t , K>::iterator it = range.first; it != range.second; ++it)
+        for (typename std::unordered_multimap<uint16_t , K>::iterator it = range.first; it != range.second; ++it)
             if ((*it).second == id) {       // if this is our block, remove it from the generation map...
                 mGenMap.erase(it);
                 break;
@@ -49,7 +49,7 @@ bool BlockCache::write(const K id, const V address) {
     } else {
         // element is not present -> check capacity
         if (mMap.size() < mCapacity) {        // we can safely insert
-            mMap[id] = BlockInfo(address);
+            mMap[id] = BlockInfo<V>(address);
             mGenMap.emplace(1, id);     // add block to generation map
             mLeast = 1;             // just inserted a generation 1 block
         } else {
@@ -57,7 +57,7 @@ bool BlockCache::write(const K id, const V address) {
             mMap.erase(leastElem->second);     // remove it
             mGenMap.erase(leastElem);           // and remove it form generation map
 
-            mMap[id] = BlockInfo(address);          // insert new block
+            mMap[id] = BlockInfo<V>(address);          // insert new block
             mGenMap.emplace(1, id);         // and place the block in generation map
             mLeast = 1;             // just inserted a generation 1 element
 
