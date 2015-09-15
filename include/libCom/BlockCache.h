ca6e802c4459ac287bd897d15076bbbdd83f252c
Steffen Klee <steffen.klee@gmail.com>
2015-09-15 19:24:50 +0200

    Moved BlockCache implementation to header file since C++ templates are somewhat special

--- include/libCom/BlockCache.h
+++ include/libCom/BlockCache.h
@@ -12,8 +12,8 @@ template<class T>
 class BlockInfo {
 
 public:
-    BlockInfo();
-    BlockInfo(const T address);
+    BlockInfo() : address(0) { };
+    BlockInfo(const T address) : address(address) { }
 
     T address;
     uint16_t generation = 1;
@@ -23,28 +23,34 @@ template<class K, class V>
 class BlockCache {
 
 public:
-    BlockCache(const uint32_t capacity = DEFAULT_CAPACITY);
+    BlockCache(const uint32_t capacity = DEFAULT_CAPACITY) : mCapacity(capacity) { }
 
     /**
      * Returns V associated with that K
      * @param id K
      * @return V of this K or object V with default constructor
      */
-    const V read(const K id) const;
+    const V read(const K id) const {
+        return mMap.find(id) != mMap.end() ? mMap.find(id)->second.address : V();
+    }
 
     /**
      * Checks if a K exists in cache
      * @param id K
      * @return True if K exists, False if not
      */
-    const bool contains(const K id) const;
+    const bool contains(const K id) const {
+        return mMap.find(id) != mMap.end();
+    }
 
     /**
      * Returns an item's current generation
      * @param id K
      * @return Generation or 0 if no such block is present
      */
-    const uint16_t generation(const K id) const;
+    const uint16_t generation(const K id) const {
+        return mMap.find(id) != mMap.end() ? mMap.find(id)->second.generation : 0;
+    }
 
     /**
      * Writes a K with its V into the cache
@@ -52,7 +58,47 @@ public:
      * @param address V
      * @return True if there was enough (no block removed), false if not
      */
-    bool write(const K id, const V address);
+    bool write(const K id, const V address) {
+        // element is present
+        auto e = mMap.find(id);
+        if (e != mMap.end()) {
+            // increase generation
+            BlockInfo<V> &block = e->second;
+            // find all elements with same generation
+            auto range = mGenMap.equal_range(block.generation);
+            // sadly we cannot use "for (auto &el: range)" here..
+            for (typename std::unordered_multimap<uint16_t , K>::iterator it = range.first; it != range.second; ++it)
+                if ((*it).second == id) {       // if this is our block, remove it from the generation map...
+                    mGenMap.erase(it);
+                    break;
+                }
+
+            // if this is a least block
+            if (block.generation == mLeast && mGenMap.find(mLeast) == mGenMap.end())       // look if there aren't any other blocks with same generation
+                mLeast++;                   // if so, adjust lowest generation
+
+            block.generation++;
+            mGenMap.emplace(block.generation, id);      // ... to insert it again in an increased generation
+        } else {
+            // element is not present -> check capacity
+            if (mMap.size() < mCapacity) {        // we can safely insert
+                mMap[id] = BlockInfo<V>(address);
+                mGenMap.emplace(1, id);     // add block to generation map
+                mLeast = 1;             // just inserted a generation 1 block
+            } else {
+                auto leastElem = mGenMap.find(mLeast);      // get one least used block id (with lowest generation). There is always one, so leastElem cannot be it::end
+                mMap.erase(leastElem->second);     // remove it
+                mGenMap.erase(leastElem);           // and remove it form generation map
+
+                mMap[id] = BlockInfo<V>(address);          // insert new block
+                mGenMap.emplace(1, id);         // and place the block in generation map
+                mLeast = 1;             // just inserted a generation 1 element
+
+                return false;       // we had to remove one block, so return false
+            }
+        }
+        return true;
+    }
 
     /**
      * Returns the current cache size
@@ -71,7 +117,11 @@ public:
     /**
      * Clears the Cache
      */
-    void clear();
+    void clear() {
+        mMap.clear();
+        mGenMap.clear();
+        mLeast = 0;
+    }
 
     /**
      * Default maximum number of elements within the cache
