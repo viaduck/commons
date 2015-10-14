55a6a0ce9c2a9805c998116680f801d0360f3534
Steffen Klee <steffen.klee@gmail.com>
2015-10-14 02:56:41 +0200

    Added default constructor to generated message classes, which generates a buffer that can hold the message
    Fixes #5

--- include/libCom/message.template.h
+++ include/libCom/message.template.h
@@ -29,7 +29,12 @@ cog.outl("#define {name}_H".format(name=name))
 [[[end]]]
 public:
     [[[cog
-        cog.outl(name+"(const Buffer &buffer) : mBuffer(buffer) {")
+        cog.outl(name+"() : mBuffer(Buffer(SIZE)) { }")
+    ]]]
+    [[[end]]]
+
+    [[[cog
+        cog.outl(name+"(Buffer &buffer) : mBuffer(buffer) {")
     ]]]
     [[[end]]]
         if (mBuffer.size() < SIZE)
@@ -40,13 +45,39 @@ public:
     [[[cog
     vars = list(g.do(filename))
 
-    cog.out(name+"(const Buffer &buffer")
+    cog.out(name+"(Buffer &buffer")
     for v in vars:
         if v[3] is not None:
             cog.out(", const {type} *_{name}".format(type=v[0], name=v[1]))
         else:
             cog.out(", {type} _{name}".format(type=v[0], name=v[1]))
     cog.out(") : {name}(buffer)".format(name=name)+" {\n")
+    offset = 0
+    for v in vars:
+        if v[3] is not None:
+            cog.outl("    memcpy(mBuffer.data({offset}), _{name}, {size});".format(offset=offset, type=v[0], name=v[1], size=v[2]*v[3]))
+            offset += v[2]*v[3]     # sizeof type * array element count
+        else:
+            cog.outl("    {name}(_{name});".format(type=v[0], name=v[1]))
+            offset += v[2]
+    ]]]
+    [[[end]]]
+    }
+
+
+
+    [[[cog
+    vars = list(g.do(filename))
+
+    cog.out(name+"(")
+    first = True
+    for v in vars:
+        if v[3] is not None:
+            cog.out((", " if not first else "")+"const {type} *_{name}".format(type=v[0], name=v[1]))
+        else:
+            cog.out((", " if not first else "")+"{type} _{name}".format(type=v[0], name=v[1]))
+        first = False
+    cog.out(") : {name}()".format(name=name)+" {\n")
     offset = 0
     for v in vars:
         if v[3] is not None:
