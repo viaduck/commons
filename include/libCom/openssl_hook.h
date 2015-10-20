8d4e59aab28d8b998f23c90a901fec7f9a3fa3a1
Alexandros Roussos <thealexroussos@gmail.com>
2015-10-21 01:14:54 +0200

    added windows include

--- include/libCom/openssl_hook.h
+++ include/libCom/openssl_hook.h
@@ -1,6 +1,10 @@
 #ifndef LIBCOM_OPENSSL_HOOK_H
 #define LIBCOM_OPENSSL_HOOK_H
 
+#ifdef __WIN32
+#include <winsock2.h>
+#endif
+
 static bool opensslInitiliazed = false;
 
 void global_initOpenSSL();
