369f28d0eedc9d963de417f95f3ae9516f388d00
Steffen Klee <steffen.klee@gmail.com>
2015-10-21 04:31:38 +0200

    Added documentation to BufferRange

--- include/libCom/BufferRange.h
+++ include/libCom/BufferRange.h
@@ -8,23 +8,48 @@
 class BufferRange {
 
 public:
+    /**
+     * Creates a BufferRange defining a range (offset, size) within a Buffer object.
+     * @param buffer Buffer object
+     * @param size Range's size
+     * @param offset Offset in Buffer to start at
+     */
     BufferRange(const Buffer &buffer, const uint32_t size, const uint32_t offset);
 
+    /**
+     * Compares two BufferRanges
+     * @param other
+     * @return True if content within the ranges is the same
+     */
     bool operator==(const BufferRange &other) const;
 
-
+    /**
+     * Getter: Buffer
+     * @return buffer
+     */
     inline const Buffer &const_buffer() const {
         return mBuffer;
     }
 
+    /**
+     * Getter: size
+     * @return size
+     */
     inline const uint32_t size() const {
         return mSize;
     }
 
+    /**
+     * Getter: offset
+     * @return offset
+     */
     inline const uint32_t offset() const {
         return mOffset;
     }
 
+    /**
+     * Getter: underlying data (of Buffer)
+     */
     inline const void *const_data() const {
         return const_buffer().const_data(offset());
     }
@@ -38,6 +63,9 @@ private:
 // taken from https://stackoverflow.com/questions/17016175/c-unordered-map-using-a-custom-class-type-as-the-key
 namespace std {
 
+    /**
+     * Implement hash function for BufferRange class (so that BufferRange is usable in a hash map)
+     */
     template<>
     struct hash<const BufferRange> {
         std::size_t operator()(const BufferRange &k) const {
