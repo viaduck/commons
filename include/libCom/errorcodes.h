2cba597987b49e5c3a8f0131e96b30bddbbd2071
Alexandros Roussos <thealexroussos@gmail.com>
2015-10-27 16:51:37 +0100

    moved errorcodes to libcom, include server status codes here

---
+++ include/libCom/errorcodes.h
@@ -0,0 +1,20 @@
+//
+// Created by pcpcpc on 27.10.2015.
+//
+
+#ifndef CORE_ERRORCODES_H
+#define CORE_ERRORCODES_H
+
+enum IOResult {
+    // internal error codes
+    ERROR_INVALID_PACKAGE,
+    ERROR_INTERNAL,
+    ERROR_INVALID_HEADER,
+
+    // server results
+    STATUS_SUCCESS = 128,
+    STATUS_NO_MESSAGES = 129,
+    STATUS_MORE_MESSAGES = 130
+};
+
+#endif //CORE_ERRORCODES_H
