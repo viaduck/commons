47793ab0378abf99c38ebf10642897e15cd6fbb5
Steffen Klee <steffen.klee@gmail.com>
2015-11-12 05:19:44 +0100

    Fixed missing import when importing conversions.h directly

--- include/libCom/conversions_crossplatform.h
+++ include/libCom/conversions_crossplatform.h
@@ -10,6 +10,8 @@
 #ifndef PORTABLE_ENDIAN_H__
 #define PORTABLE_ENDIAN_H__
 
+#include <cinttypes>
+
 #if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
 
 #	define __WINDOWS__
