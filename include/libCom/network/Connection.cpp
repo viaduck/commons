1a665e60a59ad9553f8c6287090b901563291f61
Steffen Klee <steffen.klee@gmail.com>
2016-10-29 05:01:19 +0200

    [ci skip] First part of Request rewrite
    New class name: Connection

---
+++ include/libCom/network/Connection.cpp
@@ -0,0 +1,56 @@
+#include <unistd.h>
+
+#include <libCom/openssl_hook.h>
+#include <libCom/network/Connection.h>
+
+Connection::Connection(std::string host, uint16_t port) : mHost(host), mPort(port) {
+    // TODO detect IPv6 and set protocol accordingly
+    mProtocol = Protocol::IPv4;
+
+    global_initOpenSSL();
+}
+
+Connection::~Connection() {
+    close();
+    // TODO cleanup?
+}
+
+Connection::ConnectResult Connection::connect() {
+    struct addrinfo addressQuery;
+    memset(&addressQuery, 1, sizeof(addressQuery));
+
+
+    // address query parameters
+    addressQuery.ai_family = PF_UNSPEC;
+    addressQuery.ai_socktype = SOCK_STREAM;
+    addressQuery.ai_protocol = IPPROTO_TCP;
+
+    // resolve hostname
+    struct addrinfo *addressInfoTmp = nullptr;
+    int res = getaddrinfo(mHost.c_str(), nullptr, &addressQuery, &addressInfoTmp);
+
+    std::unique_ptr<struct addrinfo, decltype(&freeaddrinfo)> addressInfo(addressInfoTmp, &freeaddrinfo);
+
+    if (res != 0)
+        return ConnectResult::ERROR_RESOLVE;
+
+    // iterate over all available addresses
+    for (struct addrinfo *it = addressInfo.get(); it; it = it->ai_next) {
+        mSocket = socket(it->ai_family, it->ai_socktype, it->ai_protocol);
+
+        // call global connect function
+        res = ::connect(mSocket, it->ai_addr, it->ai_addrlen);
+        if (res == -1)
+            ::close(mSocket);
+        else        // if there is a successful connection, return success
+            return ConnectResult::SUCCESS;
+    }
+
+    // no resolved address has been successful
+    // TODO more verbose error reporting
+    return ConnectResult::ERROR_CONNECT;
+}
+
+bool Connection::close() {
+    return false;
+}
---
+++ include/libCom/network/Connection.h
@@ -0,0 +1,92 @@
+#ifndef LIBCOM_CONNECTION_H
+#define LIBCOM_CONNECTION_H
+
+#include <cstdint>
+#include <string>
+
+#include "libCom/Buffer.h"
+
+/* network includes */
+#ifdef __WIN32
+#include <winsock2.h>
+#else
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#endif
+
+/* On Windows, socket descriptor is not an int but a #define for something else. Other OS do not know these #defines */
+#ifndef SOCKET_ERROR
+#define SOCKET int
+#define SOCKET_ERROR (-1)
+#endif
+
+/**
+ * Platform independent TCP/SSL client
+ */
+class Connection {
+public:
+    enum class ConnectResult {
+        UNKNOWN, ERROR_INTERNAL, ERROR_RESOLVE, ERROR_CONNECT, SUCCESS
+    };
+
+    enum class Status {
+        CONNECTED, UNCONNECTED, UNKNOWN
+    };
+
+    enum class Protocol {
+        IPv4,
+        IPv6
+    };
+
+    /**
+     * Creates a connection object with the specified target. Does not connect yet.
+     * @param host Hostname or IP (both 4 and 6 are supported) to connect to
+     * @param port TCP port
+     */
+    Connection(std::string host, uint16_t port);
+
+    /**
+     * Closes the connection and frees up allocated resources.
+     */
+    ~Connection();
+
+    /**
+     * Establish a connection
+     * @return Connection status
+     */
+    ConnectResult connect();
+
+    /**
+     * Closes the connection (if connected)
+     * @return Wether the connection was closed (true) or there is no active connection (false)
+     */
+    bool close();
+
+    /**
+     * @return Current connection status
+     */
+    Status status() const {
+        return mStatus;
+    }
+
+    /**
+     * @return Used IP protocol for connection
+     */
+    Protocol protocol() const {
+        return mProtocol;
+    }
+
+protected:
+    std::string mHost;
+    uint16_t mPort;
+    Protocol mProtocol;
+    Status mStatus = Status::UNKNOWN;
+
+    //
+    SOCKET mSocket;
+};
+
+
+#endif //LIBCOM_CONNECTION_H
