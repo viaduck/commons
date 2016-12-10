2aba9e9a4da03b3cb1b82576f1a5f233b211ff0c
Steffen Klee <steffen.klee@gmail.com>
2016-12-10 14:41:23 +0100

    Added EXPECT_ARRAY_NEQ custom assert

--- test/custom_assert.h
+++ test/custom_assert.h
@@ -9,9 +9,19 @@
     {\
     TARTYPE* reference_ = reinterpret_cast<TARTYPE *> (reference); \
     TARTYPE* actual_ = reinterpret_cast<TARTYPE *> (actual); \
-    for(int cmp_i = 0; cmp_i < element_count; cmp_i++ ){\
+    for(decltype(element_count) cmp_i = 0; cmp_i < element_count; cmp_i++ ){\
       EXPECT_EQ(reference_[cmp_i], actual_[cmp_i]);\
     }\
+    }
+#define EXPECT_ARRAY_NEQ(TARTYPE, reference, actual, element_count) \
+    {\
+    TARTYPE* reference_ = reinterpret_cast<TARTYPE *> (reference); \
+    TARTYPE* actual_ = reinterpret_cast<TARTYPE *> (actual); \
+    decltype(element_count) cmp_i; \
+    for(cmp_i = 0; cmp_i < element_count; cmp_i++ ){\
+      if (reference_[cmp_i] != actual_[cmp_i]) break;\
+    }\
+    ASSERT_NE(element_count, cmp_i) << "All array values are identical";\
     }
 
 #endif //PUSHCLIENT_CUSTOM_ASSERT_H
