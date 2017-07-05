5498b9a25de141672bf1eb96868e726dbe9e85c5
Steffen Klee <steffen.klee@gmail.com>
2017-07-06 01:45:12 +0200

    Extended and optimized OSFileWrapper

--- include/libCom/OSFileWrapper.h
+++ include/libCom/OSFileWrapper.h
@@ -8,20 +8,51 @@
 #include <windows.h>
 #else
 #include <unistd.h>
+#include <sys/stat.h>
 #endif
 
+
 // TODO: add tests
+/**
+ * Cross platform (POSIX, Windows) wrapper for common filesystem operations.
+ *
+ * Scheduled for removal once std::filesystem is standardized, released and well-supported.
+ */
 class OSFileWrapper {
 public:
-    static bool resizeFile(const std::string &path, FILE *&descriptor, const char *mode, size_t newSize) {
+    /**
+     * Resizes a file described by stream to newSize. This both enlarges and shrinks files.
+     * @param stream File stream. This parameter is NOT checked for validness.
+     * @param newSize File's new size
+     * @return True on success, false on failure
+     */
+    static bool resizeFile(FILE *stream, size_t newSize) {
         bool ret = false;
 #ifdef WIN32
-        fseek(descriptor, SEEK_SET, newSize);
-        ret = SetEndOfFile(_fileno(descriptor)) != 0;
+        fseek(stream, SEEK_SET, newSize);
+        ret = SetEndOfFile((HANDLE)_fileno(stream)) != 0;
 #else
-        fclose(descriptor);
-        ret = truncate(path.c_str(), newSize) == 0;
-        descriptor = fopen(path.c_str(), mode);
+        ret = ftruncate(fileno(stream), newSize) == 0;
+#endif
+        return ret;
+    }
+
+    /**
+     * Gets the size of an opened file stream.
+     * @param stream File stream. This parameter is NOT checked for validness.
+     * @return File size or 0 on failure.
+     */
+    static uint64_t size(FILE *stream) {
+        uint64_t ret = 0;
+
+#ifdef WIN32
+        LARGE_INTEGER outSize;
+        if (GetFileSizeEx((HANDLE)_fileno(stream), &outSize) != 0)
+            ret = outSize.QuadPart;
+#else
+        struct stat st;
+        if (fstat(fileno(stream), &st) == 0)
+            ret = static_cast<uint64_t>(st.st_size);
 #endif
         return ret;
     }
