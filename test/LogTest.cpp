697aeaa9733d64d5775965be131dae4811628984
Steffen Klee <steffen.klee@gmail.com>
2017-08-30 22:48:03 +0200

    Fixed issues introduced in 9370a0b53e5e447fc412cd96c4801eaac4d5ad87

--- test/LogTest.cpp
+++ test/LogTest.cpp
@@ -5,8 +5,8 @@ TEST_F(LogTest, Simple) {
     Log::dbg<<"Test 123 "<<456<<" "<<true<<"\n";
     Log::err<<"This is an error "<<std::hex<<1337<<" "<<0.0559897f<<"\n";
 
-    EXPECT_EQ("[LogLevel::DEBUG] Test 123 456 1\n", mLogger->toString(LogLevel::LEVEL_DEBUG));
-    EXPECT_EQ("[LogLevel::ERROR] This is an error 539 0.0559897\n", mLogger->toString(LogLevel::LEVEL_ERROR));
+    EXPECT_EQ("[LogLevel::LEVEL_DEBUG] Test 123 456 1\n", mLogger->toString(LogLevel::LEVEL_DEBUG));
+    EXPECT_EQ("[LogLevel::LEVEL_ERROR] This is an error 539 0.0559897\n", mLogger->toString(LogLevel::LEVEL_ERROR));
 }
 
 TEST_F(LogTest, EnableDisable) {
@@ -21,8 +21,8 @@ TEST_F(LogTest, EnableDisable) {
     // check logging functionality if enabled
     Log::dbg<<"Test 123";
     Log::err<<"1337 456";
-    EXPECT_EQ("[LogLevel::DEBUG] Test 123", mLogger->toString(LogLevel::LEVEL_DEBUG));
-    EXPECT_EQ("[LogLevel::ERROR] 1337 456", mLogger->toString(LogLevel::LEVEL_ERROR));
+    EXPECT_EQ("[LogLevel::LEVEL_DEBUG] Test 123", mLogger->toString(LogLevel::LEVEL_DEBUG));
+    EXPECT_EQ("[LogLevel::LEVEL_ERROR] 1337 456", mLogger->toString(LogLevel::LEVEL_ERROR));
     mLogger->clear();
 
     // disable logging globally
@@ -35,8 +35,8 @@ TEST_F(LogTest, EnableDisable) {
     EXPECT_FALSE(Log::err.isEnabled());
     Log::dbg<<"Test 123";
     Log::err<<"1337 456";
-    EXPECT_NE("[LogLevel::DEBUG] Test 123", mLogger->toString(LogLevel::LEVEL_DEBUG));
-    EXPECT_NE("[LogLevel::ERROR] 1337 456", mLogger->toString(LogLevel::LEVEL_ERROR));
+    EXPECT_NE("[LogLevel::LEVEL_DEBUG] Test 123", mLogger->toString(LogLevel::LEVEL_DEBUG));
+    EXPECT_NE("[LogLevel::LEVEL_ERROR] 1337 456", mLogger->toString(LogLevel::LEVEL_ERROR));
 
 
     // disable individual log level
@@ -50,6 +50,6 @@ TEST_F(LogTest, EnableDisable) {
     EXPECT_TRUE(Log::err.isEnabled());
     Log::dbg<<"Test 123";
     Log::err<<"1337 456";
-    EXPECT_NE("[LogLevel::DEBUG] Test 123", mLogger->toString(LogLevel::LEVEL_DEBUG));
-    EXPECT_EQ("[LogLevel::ERROR] 1337 456", mLogger->toString(LogLevel::LEVEL_ERROR));
+    EXPECT_NE("[LogLevel::LEVEL_DEBUG] Test 123", mLogger->toString(LogLevel::LEVEL_DEBUG));
+    EXPECT_EQ("[LogLevel::LEVEL_ERROR] 1337 456", mLogger->toString(LogLevel::LEVEL_ERROR));
 }
