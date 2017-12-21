9d30873f671fcfd2c2cd2c78aebd218426aae2dd
Steffen Klee <steffen.klee@gmail.com>
2017-12-21 02:41:36 +0100

    Added StdoutLogger as ILogger implementation

---
+++ include/commons/log/impl/StdoutLogger.h
@@ -0,0 +1,34 @@
+#ifndef VDCOMMONS_STDOUTLOGGER_H
+#define VDCOMMONS_STDOUTLOGGER_H
+
+#include <commons/log/Log.h>
+#include <ctime>
+#include <iomanip>
+
+/**
+ * This ILogger implementation logs all log levels to stdout.
+ */
+class StdoutLogger : public ILogger {
+public:
+    bool open() override {
+        return true;
+    }
+
+    void close() override {}
+
+    bool isOpen() override {
+        return true;
+    }
+
+    std::ostream &stream() override {
+        return std::cout;
+    }
+
+    bool wantsLog(LogLevel level) override {
+        std::time_t now = std::time(nullptr);
+        std::cout<<std::put_time(std::localtime(&now), "%FT%T%z")<<" ["<<level<<"] ";
+        return true;
+    }
+};
+
+#endif //VDCOMMONS_STDOUTLOGGER_H
