8a9a01fe0880b0572a5c06cfc694b175954033de
Alexandros Roussos <thealexroussos@gmail.com>
2018-04-26 14:41:58 +0200

    Added License

--- include/commons/util/Except.h
+++ include/commons/util/Except.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015-2018 The ViaDuck Project
+ *
+ * This file is part of Commons.
+ *
+ * Commons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Commons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Commons.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef COMMONS_EXCEPT_H
 #define COMMONS_EXCEPT_H
 
