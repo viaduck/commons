3f273bf14d4126a8ec1910d01cb0fbac5e012a76
Alexandros Roussos <thealexroussos@gmail.com>
2019-04-24 16:30:16 +0200

    Added file logger

---
+++ include/commons/log/impl/FileLogger.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019 The ViaDuck Project
+ *
+ * This file is part of Commons.
+ *
+ * Commons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Commons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Commons.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef COMMONS_FILELOGGER_H
+#define COMMONS_FILELOGGER_H
+
+#include <fstream>
+
+#include <commons/log/ILogger.h>
+
+/**
+ * This ILogger implementation logs all log levels to file.
+ */
+class FileLogger : public ILogger {
+public:
+    FileLogger(const std::string &filename) {
+        mFile.open(filename, std::fstream::out | std::fstream::app);
+    }
+
+    std::ostream &stream() override {
+        return mFile;
+    }
+
+    bool wantsLog(LogLevel level) override;
+
+protected:
+    std::fstream mFile;
+};
+
+#endif //COMMONS_FILELOGGER_H
---
+++ src/commons/impl/FileLogger.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2019 The ViaDuck Project
+ *
+ * This file is part of Commons.
+ *
+ * Commons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Commons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Commons.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <commons/log/impl/FileLogger.h>
+#include <commons/util/Time.h>
+
+
+bool FileLogger::wantsLog(LogLevel level) {
+    mFile << Time::nowLocal().formatFull("%Y-%m-%dT%H:%M:%S%z") << " [" << level << "] ";
+    return true;
+}
