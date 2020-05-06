be3f84d322c2de3cf195f778f4d98b9f3a6f1d7a
Alexandros Roussos <thealexroussos@gmail.com>
2020-05-06 03:37:49 +0200

    Fixed vd-lint issues

--- src/network/socket/DefaultSocketFactory.h
+++ src/network/socket/DefaultSocketFactory.h
@@ -1,5 +1,24 @@
-#ifndef CORE_DEFAULTSOCKETFACTORY_H
-#define CORE_DEFAULTSOCKETFACTORY_H
+/*
+ * Copyright (C) 2019 The ViaDuck Project
+ *
+ * This file is part of Commons.
+ *
+ * Commons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Commons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Commons.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef COMMONS_DEFAULTSOCKETFACTORY_H
+#define COMMONS_DEFAULTSOCKETFACTORY_H
 
 #include <network/socket/ISocketFactory.h>
 
@@ -12,4 +31,4 @@ public:
     }
 };
 
-#endif //CORE_DEFAULTSOCKETFACTORY_H
+#endif //COMMONS_DEFAULTSOCKETFACTORY_H
