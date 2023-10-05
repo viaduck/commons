5bd005d07a6c9f3f93e2d0b2ae194ca0e14365cb
Alexandros Roussos <thealexroussos@gmail.com>
2023-10-05 02:17:28 +0200

    Refactored network protocol and all messages

---
+++ include/network/protocol/types.h
@@ -0,0 +1,20 @@
+#ifndef COMMONS_TYPES_H
+#define COMMONS_TYPES_H
+
+#include <flatbuffers/network/data/ReqDataHeader.h>
+#include <flatbuffers/network/data/ResDataHeader.h>
+#include <flatbuffers/network/data/count/ReqMessageCount.h>
+#include <flatbuffers/network/data/count/ResMessageCount.h>
+#include <flatbuffers/network/data/get/ReqGetMessages.h>
+#include <flatbuffers/network/data/get/ResGetMessages.h>
+#include <flatbuffers/network/data/get/ResMessage.h>
+#include <flatbuffers/network/data/send/ReqSendMessage.h>
+
+#include <flatbuffers/network/push/ReqPushHeader.h>
+#include <flatbuffers/network/push/ResPushHeader.h>
+#include <flatbuffers/network/push/RepHeartbeat.h>
+#include <flatbuffers/network/push/RepPostbox.h>
+#include <flatbuffers/network/push/notify/ResNotify.h>
+#include <flatbuffers/network/push/subscribe/ReqSubUnsub.h>
+
+#endif //COMMONS_TYPES_H
